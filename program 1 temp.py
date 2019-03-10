58c6c0bbb7f587f6b79ee50eba959363b2dd4911
Oscar Hilton <oscar@Oscars-Air.lan>
2019-03-10 20:09:54 +0000

    removed sound lib

--- program 1 temp.py
+++ program 1 temp.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import math, sys, os
 import subprocess
 import socket
-import sounds
+# import sounds
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)  
@@ -36,7 +36,7 @@ def callPhoneNumber(number):
 
     service = availableNumbers.get(number)
     try:
-        sounds.playRing()
+        # sounds.playRing()
         globals()[service](inCall, number)
         inCall = True
     except KeyError:
