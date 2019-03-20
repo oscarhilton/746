e5ff510e8715f8d2a415adc99bcbd5e0943ddc4f
Oscar Hilton <oscar@Oscars-Air.lan>
2019-03-20 10:48:27 +0000

    Test

--- modules/Weather.py
+++ modules/Weather.py
@@ -2,14 +2,14 @@ from Call import Call
 from weather import Weather, Unit
 
 class Weather(Call):
-  def __init__(self, location):
+  def __init__(self, location, unit=Unit.CELSIUS):
     self.name = "Weather"
     self.options = {
       "1": "today",
       "2": "tomorrow",
       "3": "week"
     }
-    self.weather = Weather(unit=Unit.CELSIUS)
+    self.weather = Weather(unit=unit)
     self.location = self.weather.lookup_by_location(location)
   
   def today(self):
