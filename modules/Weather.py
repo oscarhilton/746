066744c56fd186a8942200155ca16dee5d2a59ea
Oscar Hilton <oscarhilton@hotmail.com>
2019-04-09 18:38:16 +0100

    AppID moved to self

--- modules/Weather.py
+++ modules/Weather.py
@@ -9,10 +9,11 @@ class Weather(Call):
       "1": "today",
       "2": "rainToday"
     }
+    self.appId = "20d6758a5860e3fb77f974e096a6f9c5"
 
   def today(self):
     location = "London"
-    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=20d6758a5860e3fb77f974e096a6f9c5'.format(location))
+    r = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}'.format(location, self.appId))
 
     data = r.json()
 
@@ -27,7 +28,7 @@ class Weather(Call):
     
   def rainToday(self):
     location = "London:
-    r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&APPID=20d6758a5860e3fb77f974e096a6f9c5'.format(location))
+    r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&APPID={}'.format(location, self.appId))
 
     data = r.json()
     forecast = data["list"]
