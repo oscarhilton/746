0b3281b44767311f6706564c4c0e49e293b69b66
Oscar Hilton <oscar@Oscars-Air.lan>
2019-04-13 16:31:36 +0100

    shutdown lights

--- modules/lights.py
+++ modules/lights.py
@@ -7,10 +7,10 @@ GPIO.setup(20, GPIO.OUT) #Light
 
 def startup(ready = True):
   i = 1
-  while i < 20 and ready:
+  while i < 3 and ready:
     try:
       GPIO.output(20, True)
-      time.sleep(0.1)
+      time.sleep(0.1 * i)
       GPIO.output(20, False)
       time.sleep(0.1)
       i += 1
@@ -18,11 +18,13 @@ def startup(ready = True):
       break
 
 def shutdown():
-  while True:
+  i = 1
+  while i < 3:
     try:
       GPIO.output(20, True)
       time.sleep(0.1)
       GPIO.output(20, False)
-      time.sleep(0.1)
+      time.sleep(0.1 * i)
+      i += 1
     except:
       break
