10508775ae4cd9f14b1dd26b95271d19a59a1351
Oscar Hilton <oscar.hilton@mobile-5.com>
2019-05-31 10:36:05 +0100

    Better microphone settings

--- modules/Microphone.py
+++ modules/Microphone.py
@@ -10,13 +10,32 @@ def printMicrophoneSources():
     for index, name in enumerate(sr.Microphone.list_microphone_names()):
         print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
 
+def say(text):
+    print(text)
+    return
+
 def listen():
-  with m as source:
-    print("Say something!")
-    audio = r.listen(source)
     try:
-        return r.recognize_google(audio)
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("A moment of silence, please...")
+        with m as source: r.adjust_for_ambient_noise(source)
+            print("Set minimum energy threshold to {}".format(r.energy_threshold))
+            while True:
+                print("Say something!")
+                with m as source: audio = r.listen(source)
+                print("Got it! Now to recognize it...")
+                try:
+                    # recognize speech using Google Speech Recognition
+                    value = r.recognize_google(audio)
+
+                    # we need some special handling here to correctly print unicode characters to standard output
+                    if str is bytes: # this version of Python uses bytes for strings (Python 2)
+                        say(u"You said {}".format(value).encode("utf-8"))
+                    else: # this version of Python uses unicode for strings (Python 3+)
+                        say("You said {}".format(value))
+                except sr.UnknownValueError:
+                    print("Oops! Didn't catch that")
+                except sr.RequestError as e:
+                    print("Uh oh! Couldn't request results from Google Speech Recognition service; {0}".format(e))
+                time.sleep(0.5) # sleep for a little bit
+                except KeyboardInterrupt:
+                    pass
